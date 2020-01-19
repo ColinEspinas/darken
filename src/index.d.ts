1a771cdf1ab2c254dcb17a8484f57ded95becb21
George Pickering <29524044+geopic@users.noreply.github.com>
2020-01-19 05:07:32 +0000

    feat: add TypeScript types declaration file

---
+++ src/index.d.ts
@@ -0,0 +1,14 @@
+export default class darken {
+  private dark: boolean;
+  constructor(options?: {
+    container: string | null;
+    default: string;
+    toggle: string | null;
+    remember: string;
+    class: string;
+    variables: object;
+  }, callback?: (active: boolean) => void);
+  toggle(): void;
+  on(): void;
+  off(): void;
+}
