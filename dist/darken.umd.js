950e299cfce9f5afc58f6c2602b91d463b50b831
ColinEspinas <colin.espinas@yahoo.fr>
2021-07-26 15:44:32 +0200

    chores: Merged new bundler branch & fixed demo

--- dist/darken.umd.js
+++ dist/darken.umd.js
@@ -96,6 +96,9 @@
 						else element.style.setProperty(key, value[this.dark ? 'dark' : 'light']);
 					}
 				}
+				// Set color-scheme to root
+				document.documentElement.style.setProperty('color-scheme', this.dark ? 'dark' : 'light');
+
 				// Set stylesheet
 				this.__changeStylesheet(options.stylesheets.id, options.stylesheets[this.dark ? 'dark' : 'light']);
 
